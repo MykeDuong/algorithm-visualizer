65b3530f40bee039c6c1463bec3fe415de0e9cd0
Minh Duong <hongminh4402@gmail.com>
2022-12-11 13:17:54 -0700

    gh-pages

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import ghpages from 'gh-pages'
+
+ghpages.publish(
+  'public', // path to public directory
+  {
+    branch: 'gh-pages',
+    repo: 'https://github.com/MykeDuong/algorithm-visualizer.git', // Update to point to your repository
+    user: {
+      name: 'MykeDuong', // update to use your name
+      email: 'hongminh4402@gmail.com' // Update to use your email
+    },
+    dotfiles: true
+  },
+  () => {
+    console.log('Deploy Complete!');
+  }
+);
